6a323461d2d111e2cfd2b1f7767c1a2c44adc8e4
Matt Parker <moonmaster9000@gmail.com>
2014-09-06 08:00:30 -0700

    TodoRepo extraction

--- todo.rb
+++ todo.rb
@@ -1,11 +1,33 @@
 #!/usr/bin/env ruby
 
+class TodoRepo
+  def initialize
+    @repo_file = "todo_list"
+  end
+
+  def destroy_all
+    File.delete(repo_file)
+  end
+
+  def create(todo_text)
+    File.write(repo_file, todo_text + "\n")
+  end
+
+  def all
+    File.read(repo_file)
+  end
+
+  private
+  attr_reader :repo_file
+end
+
+todo_repo = TodoRepo.new
 
 case ARGV.first
   when "destroy"
-    File.delete("todo_list")
+    todo_repo.destroy_all
   when "add"
-    File.write("todo_list", ARGV[1..-1].join(" "))
+    todo_repo.create(ARGV[1..-1].join(" "))
   when "list"
-    puts File.read("todo_list")
+    puts todo_repo.all
 end
