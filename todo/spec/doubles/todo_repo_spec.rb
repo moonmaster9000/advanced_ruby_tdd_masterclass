9b13deb9e4f94165b2bb19e4ef139f59171d5ee3
Matt Parker <moonmaster9000@gmail.com>
2014-09-07 14:49:47 -0700

    Backfilling an explicit test for todo repository behavior

---
+++ todo/spec/doubles/todo_repo_spec.rb
@@ -0,0 +1,52 @@
+require "doubles/in_memory_todo_repo"
+require "todo/entities/todo"
+require "doubles/valid_todo_stub"
+
+describe "A Todo Repository" do
+  it "adds todos into the repository" do
+    todo_repo.create(todo)
+
+    expect(todo_repo.all).to include todo
+  end
+
+  it "destroys all todos in the repository" do
+    todo_repo.create(todo)
+
+    todo_repo.destroy_all
+
+    expect(todo_repo).to be_empty
+  end
+
+  it "provides access to todos, dones, and all" do
+    todo_repo.create(todo)
+    todo_repo.create(done_todo)
+
+    expect(todo_repo.all).to include todo, done_todo
+
+    expect(todo_repo.done).not_to include todo
+    expect(todo_repo.done).to include done_todo
+
+    expect(todo_repo.todos).not_to include done_todo
+    expect(todo_repo.todos).to include todo
+  end
+
+  it "updates todos" do
+    todo_repo.create(todo)
+
+    todo.do!
+
+    todo_repo.update(todo)
+
+    expect(todo_repo.done).to include todo
+  end
+
+  it "finds todos by description" do
+    todo_repo.create(todo)
+    expect(todo_repo.find_by_description(todo.description)).to eq todo
+  end
+
+  let(:valid_todo_attributes) { ValidTodoStub.attributes }
+  let(:todo_repo)             { InMemoryTodoRepo.new }
+  let(:todo)                  { Todo::Entities::Todo.new(valid_todo_attributes) }
+  let(:done_todo)             { Todo::Entities::Todo.new(valid_todo_attributes).tap &:do! }
+end
